e1237ffbc3b3e9d93af5f404ece11d57b79391f2
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2022-03-01 08:36:38 +0700

    handle popup guest

--- src/Chat/Conversation.jsx
+++ src/Chat/Conversation.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import MessageBoard from './components/messageBoard/MessageBoard'
 import './conversation.css'
 
 export default function Conversation() {
+    const [showModal, setShowModal] = useState(false);
+
+    function openMessageBoard(){
+        setShowModal(!showModal);  
+    }
     return (
         <>
-        <MessageBoard />
-            <div className="bubble">
+        {showModal && <MessageBoard />}
+            <div className="bubble" onClick={openMessageBoard}>
                 <span><img src="https://img.icons8.com/ios/27/000000/lifebuoy.png" />&nbsp;Cần trợ giúp?</span>
             </div>
         </>
--- src/Chat/components/messageBoard/MessageBoard.jsx
+++ src/Chat/components/messageBoard/MessageBoard.jsx
@@ -1,11 +1,43 @@
+import React, { useState } from "react";
 import Message from "../message/Message";
-import './messageBoard.css'
+import './messageBoard.css';
 
-export default function messageBoard() {
+export default function MessageBoard() {
+  const [guestUser, setGuestUser] = useState(false);
+  const [showModal, setShowModal] = useState(true);
+
+  let guestUserEmail = React.createRef();
+
+  function popupModalGuestUser() {
+
+    setGuestUser(localStorage.getItem('user.gmail') === null ? true : false)
+    if (localStorage.getItem('user.gmail') === null) {
+      setGuestUser(true);
+      setShowModal(false);
+    }
+    if (guestUser) {
+      setGuestUserInformation();
+    }
+  }
+  function closeChat() {
+    setShowModal(false);
+  }
+
+  function closeGuestModal(){
+    setShowModal(true);
+    setGuestUser(false);
+  }
+  function setGuestUserInformation() {
+    if (guestUser) {
+      localStorage.setItem('user.gmail', JSON.stringify(guestUserEmail.current.value))
+      setGuestUser(false);
+      setShowModal(true);
+    }
+  }
+
   return (
     <>
-
-      <div className="chatBox">
+      {showModal && <div className="chatBox"  >
         <div className="chatBoxHeader">
           <div className="rightbarProfileImgContainer">
             <img className="rightbarProfileImg" src="https://i.pinimg.com/564x/21/a0/25/21a0250fa64bbca1e8b5aedcb9367093.jpg" alt="" />
@@ -13,7 +45,7 @@ export default function messageBoard() {
             <span className="rightbarUsername">Admin</span>
           </div>
           <span className="rightbarHint">Thường trả lời trong vài phút</span>
-          <img src="https://img.icons8.com/material-outlined/24/000000/delete-sign.png" style={{ right: "8px", bottom: "30px", cursor: "pointer", position: "absolute" }} />
+          <img onClick={closeChat} src="https://img.icons8.com/material-outlined/24/000000/delete-sign.png" style={{ right: "8px", bottom: "30px", cursor: "pointer", position: "absolute" }} />
         </div>
         <div className="chatBoxWrapper">
           <div className="chatBoxTop">
@@ -25,25 +57,29 @@ export default function messageBoard() {
           </div>
           <div className="chatBoxBottom">
             <textarea className='chatMessageInput' placeholder='Type something'></textarea>
-            <button className='chatSubmitButton'>Send</button>
+            <img onClick={popupModalGuestUser} src="https://img.icons8.com/external-gradak-royyan-wijaya/40/000000/external-chat-gradak-communikatok-solidarity-gradak-royyan-wijaya-9.png" style={{ right: "15px", bottom: "30px", cursor: "pointer", position: "absolute" }} />
           </div>
         </div>
-      </div>
+      </div>}
 
-      <div className="modalGuest">
-      <form>
-        <div class="form-group t">
-          <label for="guestEmail">Email address</label>
-          <input type="email" class="form-control" id="guestEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+      {guestUser && <div className="modalGuest">
+        <form onSubmit={setGuestUserInformation}>
+        <div className="modalGuestHeader">
+          <img onClick={closeGuestModal} src="https://img.icons8.com/material-outlined/24/000000/delete-sign.png" style={{ right: "8px", bottom: "5px", cursor: "pointer", position: "absolute" }} />
         </div>
-        <div class="form-group">
-          <label for="guestMessage">Câu hỏi của bạn</label>
-          <input type="text" class="form-control" id="guestMessage" placeholder="Bạn cần hỗ trợ gì?" />
-        </div>
-      </form>  
-      </div>  
-   
-
+          <div className="form-group">
+            <label hmtlFor="guestEmail">Email address</label>
+            <input required type="email" ref={guestUserEmail} className="form-control" id="guestEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+          </div>
+          <div className="form-group">
+            <label hmtlFor="guestMessage">Câu hỏi của bạn</label>
+            <input required type="text" className="form-control" id="guestMessage" placeholder="Bạn cần hỗ trợ gì?" />
+            <div className="chatSubmitButton">
+              <button type="submit">Send</button>
+            </div>
+          </div>
+        </form>
+      </div>}
     </>
   )
 }
